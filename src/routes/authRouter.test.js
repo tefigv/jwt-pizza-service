2215136eab0912cfb00f0a170b31af31bbcc1df3
tefigv <stephgv@byu.edu>
2024-10-11 16:56:55 -0600

    add(login test)

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -1,3 +1,30 @@
-test('hello world', () => {
-    expect(`hello ` + `world`).toBe(`hello world`);
+const request = require('supertest');
+const app = require('../service');
+
+const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
+let testUserAuthToken = null;
+let userID = null;
+
+beforeAll(async () => {
+  testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
+  const badTestUser = { name: 'pizza diner', email: '', password: '' };
+  const wrongResponse = await request(app).post('/api/auth').send(badTestUser);
+  expect(wrongResponse.status).toBe(400);
+
+  const registerRes = await request(app).post('/api/auth').send(testUser);
+  testUserAuthToken = registerRes.body.token;
 });
+
+test('login', async () => {
+  const loginRes = await request(app).put('/api/auth').send(testUser);
+  expect(loginRes.status).toBe(200);
+  expect(loginRes.body.token).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+
+  const {password, ...user } = { ...testUser, roles: [{ role: 'diner' }] };
+  expect(loginRes.body.user).toMatchObject(user);
+  //obligated lint checker
+  expect(testUser.password).toBe(password);
+
+ userID = loginRes.body.user.id;
+});
+
